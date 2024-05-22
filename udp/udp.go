5fef49ba51b6d6e4b2d6e923d11f2f3931c7d8ac
Azumi <azumitan67@gmail.com>
2024-05-22 12:33:30 -0700

    Update udp.go

--- udp/udp.go
+++ udp/udp.go
@@ -131,7 +131,7 @@ func PortForwardUDP(localHost string, localPort string, remoteHost string, remot
 
 	for {
 		buffer := make([]byte, bufferSize)
-		n, addr, err := udpServerConn.ReadFromUDP(buffer)
+		_, addr, err := udpServerConn.ReadFromUDP(buffer)
 		if err != nil {
 			log.Println("Error occurred while reading UDP connection:", err)
 			continue
