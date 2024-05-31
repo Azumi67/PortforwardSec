9e74fb4c00f1ac1404627eebf39318616ff8ef27
Azumi <azumitan67@gmail.com>
2024-05-31 03:21:08 -0700

    Create azumi4.go

---
+++ azumi4.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/Azumi67/PortforwardSec/udp4"
+	"github.com/klauspost/reedsolomon"
+)
+
+func main() {
+	install := flag.Bool("install", false, "Install socat")
+	var iranPort, remoteIP, remotePort, command string
+	var bufferSize int
+
+	flag.StringVar(&iranPort, "iranPort", "", "Iran port")
+	flag.StringVar(&remoteIP, "remoteIP", "", "Remote IP")
+	flag.StringVar(&remotePort, "remotePort", "", "Remote port")
+	flag.StringVar(&command, "command", "", "Custom command to use instead of socat")
+	flag.IntVar(&bufferSize, "bufferSize", 0, "Buffer size in bytes")
+
+	flag.Parse()
+
+	if *install {
+		err := udp4.installSct()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if iranPort == "" || remoteIP == "" || remotePort == "" {
+		log.Fatal("Plz provide correct inputs for iranPort, remoteKharej, and remotePort")
+	}
+
+	enc, err := reedsolomon.New(2, 2)
+	if err != nil {
+		log.Println("Error creating ReedSolomon encoder:", err)
+		return
+	}
+
+	udp4.PortFwdUDP(iranPort, remoteIP, remotePort, command, bufferSize, enc)
+}
