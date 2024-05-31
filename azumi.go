44c62d270ed05ec694b45e6a8f03b81c5d160d0d
Azumi <azumitan67@gmail.com>
2024-05-31 12:42:18 -0700

    Create azumi.go

---
+++ azumi.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Azumi67/PortforwardSec/tcp"
+)
+
+func main() {
+	if len(os.Args) != 6 {
+		fmt.Printf("Usage: %s local_host local_port remote_host remote_port protocol\n", os.Args[0])
+		return
+	}
+
+	localHost := os.Args[1]
+	localPort := os.Args[2]
+	remoteHost := os.Args[3]
+	remotePort := os.Args[4]
+	protocol := os.Args[5]
+
+	switch protocol {
+	case "tcp":
+		tcp.PortForwardTCP(localHost, localPort, remoteHost, remotePort)
+	default:
+		fmt.Println("Invalid protocol. Supported protocols are tcp and udp. More methods coming!")
+	}
+}
