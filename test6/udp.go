b3582b72a078b48718cd3ef373974f249256042e
Azumi <azumitan67@gmail.com>
2024-06-01 04:37:01 -0700

    Update udp.go

--- test6/udp.go
+++ test6/udp.go
@@ -37,65 +37,14 @@ func installSct() error {
 	return nil
 }
 
-func PortFwdUDP(localPt string, remoteKharej string, remotePort string, command string, buffer int, enc reedsolomon.Encoder) {
-	log.Printf("Azumichan is starting port forwarding: %s -> %s:%s\n", localPt, remoteKharej, remotePort)
-
-	cmd := exec.Command("socat",
-		"UDP6-LISTEN:"+localPt+",reuseaddr,fork",
-		"UDP6:["+remoteKharej+"]:"+remotePort)
-
-	if command != "" {
-		cmd = exec.Command("sh", "-c", command)
-	}
-
-	if buffer > 0 {
-		cmd.Env = append(os.Environ(), "SOCAT_SNDBUF="+strconv.Itoa(buffer))
-	}
-
-	stdoutPipe, err := cmd.StdoutPipe()
-	if err != nil {
-		log.Println("Some weird error occurred while constructing water pipe:", err)
-		return
-	}
-	stderrPipe, err := cmd.StderrPipe()
-	if err != nil {
-		log.Println("Some weird error occurred with water pipe:", err)
-		return
-	}
-
-	err = cmd.Start()
-	if err != nil {
-		log.Println("Something weird happened while starting port forwarding:", err)
-		return
-	}
-
-	scanner := bufio.NewScanner(stdoutPipe)
-	go func() {
-		for scanner.Scan() {
-			log.Println(scanner.Text())
-		}
-	}()
-
-	errScanner := bufio.NewScanner(stderrPipe)
-	go func() {
-		for errScanner.Scan() {
-			log.Println(errScanner.Text())
-		}
-	}()
-
-	err = cmd.Wait()
-	if err != nil {
-		log.Println("Got hit by a wall while running port forwarding:", err)
-	}
-}
-func PortFwdUDP(localPt string, remoteKharej string, remotePort string, command string, buffer int, enc reedsolomon.Encoder) {
-	log.Printf("Azumichan is starting port forwarding: %s -> %s:%s\n", localPt, remoteKharej, remotePort)
+func PortFwdUDP(iranPort string, remoteIP string, remotePort string, command string, buffer int, enc reedsolomon.Encoder) {
+	log.Printf("Azumichan is starting port forwarding: %s -> %s:%s\n", iranPort, remoteIP, remotePort)
 
 	maxGoro(remoteIP, remotePort)
 
 	cmd := exec.Command("socat",
 		"UDP6-LISTEN:"+localPt+",reuseaddr,fork",
-		"UDP6:["+remoteKharej+"]:"+remotePort)
+		"UDP6:["+remoteIP+"]:"+remotePort)
 
 	if command != "" {
 		cmd = exec.Command("sh", "-c", command)
